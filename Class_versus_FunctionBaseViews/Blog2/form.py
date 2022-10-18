f95cfe99b75975a8b63b79a3d48ed40bbd240cd8
miamo19 <miamoyoualeu5@gmail.com>
2022-10-18 05:12:49 +0100

    feat: outline the concept of Class and Function based views in two diff apps, blog and blog2 respectively

---
+++ Class_versus_FunctionBaseViews/Blog2/form.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    name = forms.CharField()
+    quantity = forms.CharField()
+    class Meta:
+        model = Product
+        fields = "__all__"
